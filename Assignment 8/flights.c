cfdf3ec752fba40f559cb1aeb5a19519cee4531d
Vineet Parmar <100948269+vinzi0812@users.noreply.github.com>
2022-03-12 10:37:48 +0530

    As8_2

--- Assignment 8/flights.c
+++ Assignment 8/flights.c
@@ -9,45 +9,52 @@ typedef struct air
     int seats;
     int count;
 }airline_t;
+void reset(airline_t c[5])
+{
+    for(int i=0;i<5;i++)
+        c[i].count=0;
+}
 void input(airline_t c[5])
 {
     for(int i=0;i<5;i++)
     {
-        printf("Enter the source: ");
+        printf("\nEnter the source: ");
         scanf("%s",c[i].src);
-        printf("\nEnter the destination: ");
+        printf("Enter the destination: ");
         scanf("%s",c[i].des);
-        printf("\nEnter the starting time: ");
+        printf("Enter the starting time: ");
         scanf("%d",&c[i].start);
-        printf("\nEnter the arriving time: ");
+        printf("Enter the arriving time: ");
         scanf("%d",&c[i].arrive);
-        printf("\nEnter the number of seats: ");
+        printf("Enter the number of seats: ");
         scanf("%d",&c[i].seats);
         c[i].count=0;
     }
 }
 int main()
 {
-    int d=1,temp=0,n;
+    int d=1,temp=0,n,flag=0,seat;
     airline_t c[5];
     input(c);
     char source[4], dest[4];
-    printf("Enter your source: ");
-    scanf("%s",source);
-    printf("Enter your destination: ");
-    scanf("%s",dest);
-    printf("Start\tEnd\tSeats\n");
-    for(int i=0;i<5;i++)
+    do
     {
-        if(strcmp(c[i].src,source)==0 && strcmp(c[i].des,dest)==0 && c[i].seats>0)
+        printf("\nEnter your source: ");
+        scanf("%s",source);
+        printf("Enter your destination: ");
+        scanf("%s",dest);
+        printf("\nStart\tEnd\tSeats\n");
+        for(int i=0;i<5;i++)
         {
-            printf("%d\t%d\t%d\n",c[i].start,c[i].arrive,c[i].seats);
-            c[i].count=d;
-            d++;
-            temp++;
+            if(strcmp(c[i].src,source)==0 && strcmp(c[i].des,dest)==0 && c[i].seats>0)
+            {
+                printf("%d\t%d\t%d\n",c[i].start,c[i].arrive,c[i].seats);
+                c[i].count=d;
+                d++;
+                temp++;
+            }
         }
-    }
-    if(temp==0)
+        if(temp==0)
         {
             printf("\nSorry we dont have any flights available");
         }
@@ -55,11 +62,30 @@ int main()
         {
             printf("Enter the number of flight you want to take: ");
             scanf("%d",&n);
+            printf("Enter the number of seats you want to book: ");
+            scanf("%d",&seat);
             for(int i=0;i<5;i++)
             {
-                if(c[i].count==d)
-                    printf("Your flight has been booked for %s to %s timings are as follows: %d to %d. Number of seats remaining: %d",c[i].src,c[i].des,c[i].start,c[i].arrive,--c[i].seats);
+                if(n==c[i].count)
+                {
+                    if(c[i].seats - seat >= 0)
+                    {
+                        c[i].seats -= seat;
+                        printf("Your flight has been booked for %s to %s",c[i].src,c[i].des);
+                        printf("\nTimings are follows: %d to %d",c[i].start,c[i].arrive);
+                        printf("\nNo of seats remaining: %d",c[i].seats);
+                    }
+                    else
+                    {
+                        printf("The flight does not have %d seats available",seat);
+                        printf("\nPlease select a different flight.");
+                    }
+                }
             }
         }
+        printf("\nEnter 0 to continue booking or any other number to exit: ");
+        scanf("%d",&flag);
+        reset(c);
+    } while (flag==0);
     return 0;
 }
