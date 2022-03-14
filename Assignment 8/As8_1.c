65d177c93d58ae94f0965c9965a219dc151bdc6b
Vineet Parmar <100948269+vinzi0812@users.noreply.github.com>
2022-03-14 19:23:12 +0530

    Update As8_1.c

--- Assignment 8/As8_1.c
+++ Assignment 8/As8_1.c
@@ -24,22 +24,31 @@ void input(member c[2])
 {
     for(int i=0;i<2;i++)
     {
+        printf("\n\nEnter the name of the member: ");
         scanf(" %s",c[i].name);
+        printf("Enter their age: ");
         scanf("%d",&c[i].age);
+        printf("Enter their address: ");
         scanf(" %[^\n]",c[i].addr);
+        printf("Enter active status (Y/N): ");
         scanf(" %c",&c[i].active);
+        printf("Enter Marital Status (Y/N): ");
         scanf(" %c",&c[i].married);
         if(c[i].married=='Y')
         {
+            printf("Enter name of the wife: ");
             scanf(" %s",c[i].det.fam.wife_name);
+            printf("Enter the number of children: ");
             scanf("%d",&c[i].det.fam.n_child);
             for(int i=0;i<c[i].det.fam.n_child;i++)
             {
+                printf("Enter name of the children: ");
                 scanf(" %s",c[i].det.fam.child_names[i]);
             }
         }
         else
         {
+            printf("Enter the hobby of the member: ");
             scanf(" %[^\n]",c[i].det.hobby);
         }
     }
@@ -49,6 +58,7 @@ int main()
     member c[2];
     input(c);
     printf("The list of married active players and their wives is as follows:\n");
+    printf("Name\tWife");
     for(int i=0;i<2;i++)
     {
         if(c[i].active=='Y' && c[i].married=='Y')
