7247f9f1c6a980f9342f221a9d1b70176f0328db
Vineet Parmar <100948269+vinzi0812@users.noreply.github.com>
2022-03-22 00:22:53 +0530

    Update As9_3.c

--- Assignment 9/As9_3.c
+++ Assignment 9/As9_3.c
@@ -1,11 +1,21 @@
 #include<stdio.h>
+void zero(int m,int n,int arr[m][n])
+{
+    for(int i = 0;i<m;i++)
+        for(int j=0;i<n;j++)
+            *(*(arr + i) + j) = 0;
+}
 void multiply(int m,int n ,int arr1[m][n],int a, int b, int arr2[a][b],int arr3[m][b])
 {
     if(n==a)
         for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++)
+            for(int j=0;j<b;j++)
+            {
+                int sum = 0;
                 for(int k=0;k<n;k++)
-                    *(*(arr3 + i) + j) += *(*(arr1 + i) + k) * *(*(arr2 + k) + j);
+                    sum += (*(*(arr1 + i) + k)) * (*(*(arr2 + k) + j));
+                *(*(arr3 + i) + j) = sum;
+            }
 }
 int main()
 {
@@ -16,19 +26,19 @@ int main()
     int arr1[m][n];
     for(int i=0;i<m;i++)
         for(int j=0;j<n;j++)
-            scanf("%d",&arr1[i][j]);
+            scanf("%d",(*(arr1 + i) + j));
     printf("Enter dimensions of matrix ");
-    scanf("%d %d",&m,&n);
+    scanf("%d %d",&a,&b);
     printf("Enter elements of the array: ");
     int arr2[a][b];
     for(int i=0;i<a;i++)
         for(int j=0;j<b;j++)
-            scanf("%d",&arr2[i][j]);
+            scanf("%d",(*(arr2 + i) + j));
     int arr3[m][b];
     multiply(m,n,arr1,a,b,arr2,arr3);
-    printf("The array after reversing is:\n");
+    printf("The matrix after multiplying is:\n");
     for(int i=0;i<m;i++)
         for(int j=0;j<b;j++)
-            printf("%d ",*(*(arr3 + i)+j));
+            printf("%d ", *(*(arr3 + i) + j));
     return 0;
 }
