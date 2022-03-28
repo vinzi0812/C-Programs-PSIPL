37d5ffbdaebbeeae8ebb0079dfccdfa378f0f26c
Vineet Parmar <100948269+vinzi0812@users.noreply.github.com>
2022-03-28 15:23:31 +0530

    Create As10_1.c

---
+++ Assignment 10/As10_1.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<string.h>
+typedef struct vehicle
+{
+    char reg[7];
+    char name[30];
+    char addr[30];
+}vehicle_t;
+void print(char filename[])
+{
+    FILE *fp;
+    vehicle_t v;
+    int n,flag=1;
+    char rg[6];
+    printf("Enter the number of records you want to search: ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        fp = fopen(filename,"r");
+        printf("\nEnter the registration number: ");
+        scanf(" %s",rg);
+        while(fscanf(fp,"%[^,],%[^,],%[^\n]\n",v.reg,v.name,v.addr)!=EOF)
+            if(strcmp(v.reg,rg)==0)
+            {
+                printf("%s %s %s\n",v.reg,v.name,v.addr);
+                flag=0;
+                break;
+            }
+        if (flag)
+            printf("Not Found\n");
+        fclose(fp);
+    }
+}
+int main()
+{
+    char filename[20];
+    printf("Enter the name of the file: ");
+    scanf(" %s",filename);
+    print(filename);
+    return 0;
+}
