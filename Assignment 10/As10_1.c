f1beb962c3048baab5db8ded406bec75727289af
Vineet Parmar <100948269+vinzi0812@users.noreply.github.com>
2022-03-28 15:51:52 +0530

    Update As10_1.c

--- Assignment 10/As10_1.c
+++ Assignment 10/As10_1.c
@@ -9,26 +9,32 @@ typedef struct vehicle
 void print(char filename[])
 {
     FILE *fp;
-    vehicle_t v;
-    int n,flag=1;
-    char rg[6];
-    printf("Enter the number of records you want to search: ");
-    scanf("%d",&n);
-    for(int i=0;i<n;i++)
+    fp=fopen(filename,"r");
+    if (fp == NULL)
+        printf("FILE NOT FOUND!\n");
+    else
     {
-        fp = fopen(filename,"r");
-        printf("\nEnter the registration number: ");
-        scanf(" %s",rg);
-        while(fscanf(fp,"%[^,],%[^,],%[^\n]\n",v.reg,v.name,v.addr)!=EOF)
-            if(strcmp(v.reg,rg)==0)
-            {
-                printf("%s %s %s\n",v.reg,v.name,v.addr);
-                flag=0;
-                break;
-            }
-        if (flag)
-            printf("Not Found\n");
-        fclose(fp);
+        vehicle_t v;
+        int n,flag=1;
+        char rg[7];
+        printf("Enter the number of records you want to search: ");
+        scanf("%d",&n);
+        for(int i=0;i<n;i++)
+        {
+            fp = fopen(filename,"r");
+            printf("\nEnter the registration number: ");
+            scanf(" %s",rg);
+            while(fscanf(fp,"%[^,],%[^,],%[^\n]\n",v.reg,v.name,v.addr)!=EOF)
+                if(strcmp(v.reg,rg)==0)
+                {
+                    printf("%s %s %s\n",v.reg,v.name,v.addr);
+                    flag=0;
+                    break;
+                }
+            if (flag)
+                printf("Not Found\n");
+            fclose(fp);
+        }
     }
 }
 int main()
