49b814721cc487b4dbc0bb740ead75e51008279b
Vineet Parmar <100948269+vinzi0812@users.noreply.github.com>
2022-04-01 12:56:03 +0530

    Update As10_2.c

--- Assignment 10/As10_2.c
+++ Assignment 10/As10_2.c
@@ -6,42 +6,44 @@ typedef struct vehicle
     char name[30];
     char addr[30];
 }vehicle_t;
-void print(char filename[])
+int print(char filename[])
 {
     FILE *fp;
     fp=fopen(filename,"r");
     if (fp == NULL)
-        printf("FILE NOT FOUND!\n");
-    else
     {
-        vehicle_t v;
-        int n,flag=1;
-        char rg[7];
-        printf("Enter the number of records you want to search: ");
-        scanf("%d",&n);
-        for(int i=0;i<n;i++)
-        {
-            fp = fopen(filename,"r");
-            printf("\nEnter the registration number: ");
-            scanf(" %s",rg);
-            while(fscanf(fp,"%[^,],%[^,],%[^\n]\n",v.reg,v.name,v.addr)!=EOF)
-                if(strcmp(v.reg,rg)==0)
-                {
-                    printf("%s %s %s\n",v.reg,v.name,v.addr);
-                    flag=0;
-                    break;
-                }
-            if (flag)
-                printf("Not Found\n");
-            fclose(fp);
-        }
+        printf("FILE NOT FOUND!\n");
+        return 0;
     }
+    vehicle_t v;
+    int n,flag=1;
+    char rg[7];
+    printf("Enter the number of records you want to search: ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        fp = fopen(filename,"r");
+        printf("\nEnter the registration number: ");
+        scanf(" %s",rg);
+        while(fscanf(fp,"%[^,],%[^,],%[^\n]\n",v.reg,v.name,v.addr)!=EOF)
+            if(strcmp(v.reg,rg)==0)
+            {
+                printf("%s %s %s\n",v.reg,v.name,v.addr);
+                flag=0;
+                break;
+            }
+        if (flag)
+            printf("Not Found\n");
+        fclose(fp);
+    }
+    return 0;
 }
 int main()
 {
+    int i;
     char filename[20];
     printf("Enter the name of the file: ");
     scanf(" %s",filename);
-    print(filename);
+    i = print(filename);
     return 0;
 }
